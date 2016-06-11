d5a4e069252c021a7fa419d157716887e51f5a86
Joel Leineweber <jleineweber@ur.local>
2016-06-11 15:57:28 -0400

    getting the UI started

--- bible_parse.py
+++ bible_parse.py
@@ -1,6 +1,14 @@
 import spacy
+import json
 nlp = spacy.load('en')
 
+### SETTINGS ###
+PRINT_TREE = False
+PRINT_JSON = True
+PRINT_TRIPLES = True
+
+# class to store and manipulate triple data
+# generally this includes subject, verb, object triples
 class Triple():
     def __init__(self, subj=None, verb=None, obj=None):
         self.subj = subj
@@ -8,20 +16,63 @@ class Triple():
         self.obj = obj
 
     def __unicode__(self):
-        return '------------------\n%s -> %s -> %s\n------------------' % (self.subj, self.verb, self.obj)
+        if self.subj or self.verb or self.obj:
+            return '------------------\n%s -> %s -> %s' % (self.subj, self.verb, self.obj)
 
     def __str__(self):
         return self.__unicode__()
 
+    def printobj(self):
+        str = self.__unicode__()
+        if str:
+            print str
+
+class JsonPrinter():
+    def __init__(self):
+        self.obj = []
+
+    def add_sentence(self):
+        self.obj.append({
+            'words': [],
+            'root': None
+        })
+
+    def add_word(self, token):
+        self.obj[-1]['words'].append({
+            'word': unicode(token),
+            'pos': token.pos_,
+            'lemma': token.lemma_,
+            'dep': token.dep_,
+            'tag': token.tag_,
+            'whitespace': token.whitespace_,
+            'isPunct': token.is_punct
+        })
+
+    def toJson(self):
+        return json.dumps(self.obj, indent=4)
+
+    def toFile(self):
+        jsonfile = open('verse_data.json', 'w')
+        jsonfile.write(self.toJson())
+        jsonfile.close()
+
+    def __unicode__(self):
+        return self.toJson()
+
+    def __str__(self):
+        return self.toJson()
+
+
 verbs = ['root', 'ccomp', 'relcl']
 subjects = ['nsubj']
 objects = ['acomp','dobj']
-def make_triple(token, parent_triple=None, level=0, conj_type=None):
-    triple = None
-    #print token.dep_
+
+def make_triple(token, parent_triple=None, level=0, conj_type=None, json_printer=None):
+    triple = Triple()
+    triples = [triple]
+
     if token.dep_.lower() in verbs:
-        triple = Triple(verb=token)
-        triples = [triple]
+        triple.verb = token
 
     for child in token.children:
         if child.dep_ in subjects:
@@ -33,73 +84,55 @@ def make_triple(token, parent_triple=None, level=0, conj_type=None):
             # handle multiple subjects
             if token.dep_ in subjects or conj_type in subjects:
                 trip = Triple(subj=child, verb=parent_triple.verb, obj=parent_triple.obj)
-                print trip
+                if PRINT_TRIPLES: trip.printobj()
 
             # handle multiple objects
             elif token.dep_ in objects or conj_type in objects:
                 trip = Triple(subj=parent_triple.subj, verb=parent_triple.verb, obj=child)
-                print trip
+                if PRINT_TRIPLES: trip.printobj()
 
-    if triple: print triple
+    if PRINT_TRIPLES and triple: triple.printobj()
 
     for child in token.children:
         lev_str = ' - '
         for i in range(level):
             lev_str += ' - '
-        print u'%s%s (%s : %s)' % (lev_str, unicode(child), child.dep_, child.pos_.lower())
+        if PRINT_TREE:
+            print u'%s%s (%s : %s)' % (lev_str, unicode(child), child.dep_, child.pos_.lower())
 
         child_conj_type = None
         if child.dep_ == 'conj':
             child_conj_type = conj_type or token.dep_
             triple = parent_triple
-        make_triple(child, triple, level+1, child_conj_type)
+        make_triple(child, triple, level+1, child_conj_type, json_printer=json_printer)
 
     #return triples
 
-def parse_chapter(filename):
+def parse_chapter(filename, json_printer):
     text = open(filename).read().decode('utf8')
     text = text.replace('\n', '')
     doc = nlp(text)
+    #print json.dumps(doc)
+    #print '-----------------------------'
     triples = []
 
     for sent in doc.sents:
         root = sent.root
-        print u'\n\n------------------------\n%s (%s : %s)' % (unicode(root), root.dep_.lower(), root.pos_.lower())
-        make_triple(sent.root)
-        '''triple = Triple(verb=unicode(sent.root))
-        for child in sent.root.children:
-            if child.dep_ == 'nsubj':
-                triple.subj = child
-            elif child.dep_ == 'acomp' or child.dep_ == 'dobj':
-                triple.obj = child
+        json_printer.add_sentence()
 
-            elif child.dep_ == 'ccomp':
-                trip = Triple(verb=child)
+        for token in sent:
+            json_printer.add_word(token)
 
-            print u' - %s (%s)' % (unicode(child), child.dep_)
+        if PRINT_TREE:
+            print u'\n\n------------------------\n%s (%s : %s)' % (unicode(root), root.dep_.lower(), root.pos_.lower())
 
-            for ch in child.children:
-                if child.dep_ == 'acomp' or child.dep_ == 'dobj':
-                    if ch.dep_ == 'conj':
-                        trip = Triple(subj=triple.subj, verb=triple.verb, obj=ch)
-                        print trip
-                        triples.append(trip)
-                print u' - - %s (%s)' % (unicode(ch), ch.dep_)
-
-                for i in ch.children:
-                    print u' - - - %s (%s)' % (unicode(i), i.dep_)
-            #print child.dep_
-
-        print triple
-        triples.append(triple)'''
-
-#    for token in doc:
-#        print token
-
-#    for token in tokens:
-#        print token
+        make_triple(sent.root, json_printer=json_printer)
 
     return doc
 
 ## run
-parse_chapter('bible-tests.txt')
+#parse_chapter('data/bible-tests.txt')
+json_printer = JsonPrinter()
+parse_chapter('data/genesis-1.txt', json_printer)
+print '-----------------------------'
+json_printer.toFile()
--- firebase/public/js/app.js
+++ firebase/public/js/app.js
@@ -1,12 +1,33 @@
 var app = angular.module("exploreBible", ["firebase"]);
-app.controller("BibleCtrl", function($scope, $firebaseArray) {
+app.controller("BibleCtrl", function($scope, $firebaseArray, $http) {
   var ref = firebase.database().ref().child("messages");
+  var sentRef = firebase.database().ref().child("sentences");
+  $scope.sents = $firebaseArray(sentRef);
+  /*$http({
+      method : "GET",
+      url : "/data/verse_data.json"
+  }).then(function mySuccess(response) {
+      angular.forEach(response.data, function(item) {
+        $scope.sents.$add(item);
+        console.log(item);
+      });
+
+  }, function myError(response) {
+      console.log(response.statusText);
+  });*/
+
+  $scope.selectWord = function(word, sent) {
+    console.log(word);
+    $scope.selectedWord = word;
+    $scope.selectedSent = sent;
+    console.log(sent);
+  };
+
   // create a synchronized array
   $scope.messages = $firebaseArray(ref);
   // add new items to the array
   // the message is automatically added to our Firebase database!
   $scope.addMessage = function() {
-    console.log($scope.newMessageText);
     $scope.messages.$add({
       text: $scope.newMessageText
     });
